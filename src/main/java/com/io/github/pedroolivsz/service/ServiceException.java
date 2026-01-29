fa6689e036965dfd20d46d1335617ed7619f90d1
João Pedro <joaopedropereira2209@gmail.com>
2026-01-29 17:45:10 -0300

    feat(serviceexception): new exceptions type for service classes

---
+++ src/main/java/com/io/github/pedroolivsz/service/ServiceException.java
@@ -0,0 +1,7 @@
+package com.io.github.pedroolivsz.service;
+
+public class ServiceException extends RuntimeException {
+    public ServiceException(String message) {
+        super(message);
+    }
+}
